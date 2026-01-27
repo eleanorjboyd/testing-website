440fbdda2dec9079317ce7a2161d87311595413c
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2026-01-27 10:51:05 -0800

    updates to the website

--- weather/urls.py
+++ weather/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search_weather, name='search_weather'),
+    path('random/', views.random_weather, name='random_weather'),
+    path('about/', views.about, name='about'),
 ]
